05d21c00b27f07cfd3fbe87c9364619735ddc7bd
Carol Nichols <carol.nichols@gmail.com>
2014-12-21 22:18:32 -0500

    String vs &str is going to be the death of me

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
+    let int_x = 3i;
+    let int_to_string = int_x.to_string();
+    let int_to_str = int_to_string.as_slice();
+
     println!("Hello, world!");
+}
+
+fn int_to_string(x: int) -> String {
+    x.to_string()
+}
+
+fn str_to_int(x: &str) -> int {
+    from_str::<int>(x).unwrap()
 }
